28dc98c13dda6b90540c04b6ea935879955c1d5a
Maykon Deykon <maykondeykon@gmail.com>
2020-07-28 20:51:43 -0300

    Adicionando router

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 // import ButtonLink from '../../components/ButtonLink';
@@ -7,11 +8,11 @@ import Button from '../../components/Button';
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" alt="MkDkFlix" src={Logo} />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">Novo vídeo</Button>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">Novo vídeo</Button>
         </nav>
         );
 }
